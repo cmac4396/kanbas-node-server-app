91d8bfdfd145ff7da2406c786b69ec1a0df33a5a
Candice Mac <65557180+cmac4396@users.noreply.github.com>
2024-04-16 00:03:45 -0400

    courses and modules fix

--- Kanbas/courses/dao.js
+++ Kanbas/courses/dao.js
@@ -1,11 +1,11 @@
 import model from "./model.js";
-export const createUser = (user) => {
-    delete user._id
-    return model.create(user);
+export const createCourse = (course) => {
+    delete course._id
+    return model.create(course);
   }  
-export const findAllUsers = () => model.find();
-export const findUserById = (userId) => model.findById(userId);
-export const findUserByUsername = (username) =>  model.findOne({ username: username });
-export const findUserByCredentials = (username, password) =>  model.findOne({ username, password });
-export const updateUser = (userId, user) =>  model.updateOne({ _id: userId }, { $set: user });
-export const deleteUser = (userId) => model.deleteOne({ _id: userId });
+export const deleteCourse = (courseId) => model.deleteOne({ _id: courseId });
+export const updateCourse = (courseId, course) =>  model.updateOne({ _id: courseId }, { $set: course });
+export const findCourseById = (courseId) => model.findById(courseId);
+export const findAllCourses = () => model.find();
+
+
